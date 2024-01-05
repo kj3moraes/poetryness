044e9074713a00c32e290ec70cc1e338b1944dc9
Keane Moraes <lordvader3002@gmail.com>
2024-01-05 15:42:21 +0400

    docs(README): added info about usage

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # poetryness
+
 Script to automate poetry's setup from existing projects.
+
+## Usage
+
+This script will take an input directory that you want parsed and an output directory where the files 
+will be written to.
+
+* Will generate a requirements.txt file
+* Will generate a pyproject.toml, poetry.lock file and a .venv directory in the output_dir_path
+
+## Running the script
+
+To use the script, simply navigate to this directory
+
+```bash
+./infer.py <input_dir_path> <output_dir_path>
+```
+
+Copy the infer.py to `~/.local/bin` and then you can run this anywhere on your system.
--- infer.py
+++ infer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import os
 import sys
@@ -25,47 +25,55 @@ build-backend = "poetry.core.masonry.api"
 
 
 def extract_imports(file_path):
-    """ Extract import statements from a Python file. """
-    with open(file_path, 'r') as file:
+    """Extract import statements from a Python file."""
+    with open(file_path, "r") as file:
         file_content = file.read()
-    imports = re.findall(r'^\s*(?:from\s+(\w+)|import\s+(\w+))', file_content, re.MULTILINE)
+    imports = re.findall(
+        r"^\s*(?:from\s+(\w+)|import\s+(\w+))", file_content, re.MULTILINE
+    )
     return {group for import_group in imports for group in import_group if group}
 
+
 def find_external_modules(directory):
-    """ Find external modules imported in all Python files within the given directory. """
+    """Find external modules imported in all Python files within the given directory."""
     standard_libs = sys.stdlib_module_names
     external_modules = set()
 
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswith(".py"):
                 file_path = os.path.join(root, file)
                 imports = extract_imports(file_path)
                 external_modules.update(imports - standard_libs)
 
     return external_modules
 
-def create_requirements(directory, output_file='requirements.txt'):
-    """ Create requirements.txt for a given directory. """
+
+def create_requirements(directory, output_file="requirements.txt"):
+    """Create requirements.txt for a given directory."""
     external_modules = find_external_modules(directory)
-    with open(output_file, 'w') as file:
+    with open(output_file, "w") as file:
         for module in sorted(external_modules):
-            file.write(module + '\n')
+            file.write(module + "\n")
 
 
 if len(sys.argv) < 3:
-    print("You must 2 arguments.\nUSAGE: python3 infer.py <input_dir_path> <output_dir_path>")
+    print(
+        "You must 2 arguments.\nUSAGE: python3 infer.py <input_dir_path> <output_dir_path>"
+    )
     exit(1)
 
 input_dir_path = Path(sys.argv[1])
 output_dir_path = Path(sys.argv[2])
 
 # Create the requirements file in the output location
-create_requirements(input_dir_path, output_file=(output_dir_path / 'requirements.txt'))
+create_requirements(input_dir_path, output_file=(output_dir_path / "requirements.txt"))
 
 # Make a dummy pyproject.toml file
 with open(output_dir_path / "pyproject.toml", "w+") as outfile:
     outfile.write(TEMPLATE_TOML)
 
 # Use the requirements to populate the pyproject.toml
-subprocess.run('cat requirements.txt | xargs poetry add', shell=True, cwd=output_dir_path)
+subprocess.run(
+    "cat requirements.txt | xargs poetry add", shell=True, cwd=output_dir_path
+)
